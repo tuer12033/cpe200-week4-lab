570ec53fe23127f41a7c05053945539a265c3c56
tuer12033 <tour12033@hotmail.com>
2016-09-14 11:17:58 +0700

    Update BinaryCalculator.java

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -2,38 +2,89 @@ package cpe200;
 
 
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
 
     public BinaryCalculator() {
+
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand=operand;
     }
 
 
     public void setSecondOperand(Operand operand) {
+        secondOperand=operand;
     }
 
     public String add() {
-        return null;
+            double x;
+            x = Double.parseDouble(firstOperand.operand) + Double.parseDouble(secondOperand.operand);
+            if(x==(int)x) {
+                int y=(int)x;
+                return Integer.toString(y);
+            }
+            else{
+            return Double.toString(x);
+            }
     }
 
     public String subtract() {
-        return null;
-    }
+        double x;
+        x = Double.parseDouble(firstOperand.operand) - Double.parseDouble(secondOperand.operand);
+        x = (double)Math.round(x*100000d)/100000d;
+        if(x==(int)x) {
+            int y=(int)x;
+            return Integer.toString(y);
+        }
+        else{
+            return Double.toString(x);
+        }}
+
 
     public String multiply() {
-        return null;
+        double x;
+        x = Double.parseDouble(firstOperand.operand) * Double.parseDouble(secondOperand.operand);
+        if(x==(int)x) {
+            int y=(int)x;
+            return Integer.toString(y);
+        }
+        else{
+            return Double.toString(x);
+        }
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws ArithmeticException {
-        return null;
+        double x = Double.parseDouble(firstOperand.operand);
+        double y = Double.parseDouble(secondOperand.operand);
+
+        if(y == 0.0 || y == 0){
+            throw new ArithmeticException();
+        }
+        double z = x / y;
+        z = (double)Math.round(z*100000d)/100000d;
+        if(z != (int)z ){
+            return Double.toString(z) ; }
+        else{
+            int k;
+            k = (int)z;
+            return Integer.toString(k);
+        }
     }
 
     public String power() {
-        return null;
+        double x;
+        x=Math.pow(Double.parseDouble(firstOperand.operand),Double.parseDouble(secondOperand.operand));
+        if(x==(int)x) {
+            int y=(int)x;
+            return Integer.toString(y);
+        }
+        else{
+            return Double.toString(x);
+        }
+
     }
 
 }
