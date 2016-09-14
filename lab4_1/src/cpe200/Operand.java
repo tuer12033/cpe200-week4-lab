34e928a6837686f41c2fe9e4645baf8334a96c05
tuer12033 <tour12033@hotmail.com>
2016-09-14 11:18:23 +0700

    Update Operand.java

--- lab4_1/src/cpe200/Operand.java
+++ lab4_1/src/cpe200/Operand.java
@@ -1,17 +1,19 @@
 package cpe200;
 
 public class Operand {
-    public String operand;
+    protected String operand;
 
     public Operand(String operand) {
         this.operand = operand;
     }
 
     public Operand(int operand) {
+        {this.operand=Integer.toString(operand);}
 
     }
 
     public Operand(double operand) {
+        this.operand=Double.toString(operand);
 
     }
 
